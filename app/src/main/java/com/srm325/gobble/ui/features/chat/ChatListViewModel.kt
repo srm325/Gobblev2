1b8997c02ea128941066f7aaccd1791287097255
SRM <nnguy22@u.rochester.edu>
2020-11-16 23:54:39 -0500

    Initial commit

---
+++ app/src/main/java/com/srm325/gobble/ui/features/chat/ChatListViewModel.kt
@@ -0,0 +1,7 @@
+package com.srm325.gobble.ui.features.chat
+
+import androidx.lifecycle.ViewModel
+
+class ChatListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/srm325/gobble/ui/features/menu/MenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.srm325.gobble.ui.features.menu
+
+import androidx.lifecycle.ViewModel
+
+class MenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
